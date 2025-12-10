c388bca613c23fc6c849f692bfad4ba887f5a5f8
iharsh220 <hgohil220@gmail.com>
2025-12-10 10:41:39 +0530

    create task work with assigned task

--- controller/managerAssignedController/managerAssignedController.js
+++ controller/managerAssignedController/managerAssignedController.js
@@ -1151,7 +1151,7 @@ const createTask = async (req, res) => {
         const workRequest = workRequestResult.data[0];
 
         // Check if work request is accepted
-        if (workRequest.status !== 'accepted') {
+        if (workRequest.status !== 'accepted' || workRequest.status !== 'assigned') {
             return res.status(400).json({
                 success: false,
                 error: 'Work request must be accepted before creating tasks'
