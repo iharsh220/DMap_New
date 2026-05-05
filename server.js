5d91375fe9cccc3781bb9edc1857b10426377a84
iharsh220 <hgohil220@gmail.com>
2026-05-05 11:08:01 +0530

    updated port as per prod

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.use('*', (req, res) => {
     res.status(404).json({ error: 'Route not found' });
 });
 
-const PORT = process.env.PORT || 1007;
+const PORT = process.env.PORT || 1005;
 
 // Connect to database
 connectDB();
