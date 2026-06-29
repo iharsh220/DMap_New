f68ba28ca8c347e865a461570441584c0f1d12ce
iharsh220 <hgohil220@gmail.com>
2026-06-29 10:25:11 +0530

    email updated weekly and 2 time a day..!!

--- config/mailConfig.js
+++ config/mailConfig.js
@@ -4,11 +4,15 @@ require('dotenv').config();
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: process.env.EMAIL_PORT == 465, // true for 465, false for other ports
+  secure: process.env.EMAIL_PORT == 465,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  pool: true,
+  maxConnections: 5,
+  rateLimit: 10,
+  maxMessages: 100,
 });
 
 module.exports = transporter;
